91a4002343c905f82d57ed1a4b035463d2ab095d
chrisi <chrisi@chrisi.com>
2023-03-16 22:01:55 +0100

    Adding zsh-z to zsh

---
+++ nvim/init.lua
@@ -0,0 +1,30 @@
+--NeoVim config
+
+--Activates line number
+vim.opt.number = true
+
+--Ignore uppercase letter when searching
+vim.opt.ignorecase = true
+
+--Highlights the result of the last search (anoying)
+vim.opt.hlsearch = true
+
+--Wraps long lines
+vim.opt.wrap = true
+
+--Preserves intentation of wraped (virtual) lines
+vim.opt.breakindent = true
+
+--Amount of spaces equal tab
+vim.opt.tabstop = 8
+
+--Amount of spaces neovim will intdent with << >> automaticly
+vim.opt.shiftwidth = 8
+
+--Expand tabs to spaces
+vim.opt.expandtab = true
+
+
+--Keybindings
+
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -14,7 +14,12 @@ SAVEHIST=10000
 #Sourcing plugins
 source $ZDOTDIR/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source $ZDOTDIR/plugins//zsh-autosuggestions/zsh-autosuggestions.zsh
+source $ZDOTDIR/plugins//zsh-z/zsh-z.plugin.zsh
 source $ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh #must be loaded last
+
+#Z stuff
+autoload -U compinit && compinit
+zstyle ':completion:*' menu select
 
 #Aliases
 alias config='/usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME'
