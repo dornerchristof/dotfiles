fe3908ec06dfb756dc299ca8f33f4a79a91b8a20
Christof Dorner <christof@dorner.ac>
2024-10-05 13:48:24 +0200

    Getting hypridle & hyprlock working and adding gdm as a display manager and waybar as a status bar

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,13 +1,20 @@
 { config, pkgs, ... }:
 {
 	environment.systemPackages = with pkgs; [
-		greetd
-		qtgreet
-
+		
 	];
 
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true;
-security.pam.services.hyprlock = true;
-programs.hyprlock.enable = true;
+  	services.gnome.gnome-keyring.enable = true;
+  	security.pam.services.login.enableGnomeKeyring = true;
+	security.pam.services.hyprlock = {};
+	programs.hyprlock.enable = true;
+	services.hypridle.enable = true;
+	services.xserver.displayManager.gdm.enable = true;
+services.xserver.displayManager.gdm.wayland = true;
+programs.waybar.enable = true;
+services.xserver.enable = true;
+environment.sessionVariables = rec {
+NIXOS_OZONE_WL=1;
+
+};
 }
